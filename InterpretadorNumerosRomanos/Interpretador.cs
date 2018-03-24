35058d08d47cd77e5d423c9bb15857681e12fc6c
tiago.takahashi <tiago.takahashi@hbsis.com.br>
2018-03-24 15:09:25 -0300

    Soluções dojo interpretador números romanos

---
+++ InterpretadorNumerosRomanos/Interpretador.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace InterpretadorNumerosRomanos
+{
+    public class Interpretador
+    {
+        private IDictionary<char, int> _numerosRomanos = 
+            new Dictionary<char, int>() { { 'I' , 1 }, { 'V', 5 }, { 'X', 10 } , { 'L', 50 }, { 'C', 100 } , { 'D', 500 }, { 'M', 1000 }, { '\0', 0 } };        
+
+        public int Converter(string entrada)
+        {
+            //Resposta Adriano
+            //var letras = entrada.ToCharArray();
+            //var soma = 0;
+            //var anterior = 0;
+
+            //foreach (var letra in letras)
+            //{
+            //    var atual = _numerosRomanos[letra];
+            //    soma += anterior < atual ? atual - (anterior * 2) : atual;
+            //    anterior = atual;
+
+            //}
+            //return soma;
+
+            //Resposta Tiago
+            //var letras = entrada.ToCharArray();
+            //resposta 1
+            //var resultado = new List<int>();
+            //var contador = 0;
+            //do
+            //{
+            //    var atual = _numerosRomanos[letras[contador]];
+            //    if (contador > 0)
+            //    {
+            //        var anterior = _numerosRomanos[letras[contador - 1]];                    
+            //        if (atual > anterior)
+            //        {
+            //            resultado.RemoveAt(resultado.LastIndexOf(anterior));
+            //            resultado.Add(anterior * -1);
+            //        }
+            //    }
+            //    resultado.Add(atual);
+            //    contador++;
+            //} while (contador < letras.Length);                      
+            //return resultado.Sum();
+
+            //resposta 2
+            //var letras = entrada.ToCharArray();
+            //var resultado = new List<int>();
+            //letras.ToList().ForEach(letra =>
+            //{
+            //    var atual = _numerosRomanos[letra];                
+            //    var anterior = _numerosRomanos[letras.TakeWhile(x => !x.Equals(letra)).LastOrDefault()];
+            //    //Se o atual é maior que o anterior então o anterior deveria ficar negativo                
+            //    if (anterior !=  0 && atual > anterior)
+            //    {
+            //        resultado.RemoveAt(resultado.LastIndexOf(anterior));
+            //        resultado.Add(anterior * -1);
+            //    }
+            //    resultado.Add(atual);                                    
+            //});
+            //return resultado.Sum();
+
+            //resposta 3
+            var letras = entrada.ToCharArray();            
+            var resultado = letras.Select(letra =>
+            {
+                var atual = _numerosRomanos[letra];
+                var proximo = _numerosRomanos[letras.SkipWhile(x => !x.Equals(letra)).Skip(1).FirstOrDefault()];                
+                if (atual < proximo)                
+                    return atual * -1;
+
+                return atual;
+            });
+            return resultado.Sum();
+        }
+    }
+}
---
+++ InterpretadorNumerosRomanosTestes/Teste.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using InterpretadorNumerosRomanos;
+
+namespace InterpretadorNumerosRomanosTestes
+{
+    [TestFixture]
+    public class Teste
+    {
+        [Test]
+        public void deve_entender_o_numero_I_como_1()
+        {
+            var resultado = new Interpretador().Converter("I");
+            resultado.Should().Be(1);
+        }
+
+        [Test]
+        public void deve_entender_o_numero_V_como_5()
+        {
+            var resultado = new Interpretador().Converter("V");
+            resultado.Should().Be(5);
+        }
+
+        [Test]
+        public void deve_entender_o_numero_X_como_10()
+        {
+            var resultado = new Interpretador().Converter("X");
+            resultado.Should().Be(10);
+        }
+
+        [TestCase("XX", 20)]
+        [TestCase("II", 2)]
+        public void deve_entender_dois_numeros_romanos_iguais(string entrada, int resultadoEsperado)
+        {
+            var resultado = new Interpretador().Converter(entrada);
+            resultado.Should().Be(resultadoEsperado);
+        }
+
+        [TestCase("XXII", 22)]
+        [TestCase("XV", 15)]
+        [TestCase("XXV", 25)]
+        [TestCase("LIII", 53)]        
+        public void deve_entender_numeros_romanos_diferentes(string entrada, int resultadoEsperado)
+        {
+            var resultado = new Interpretador().Converter(entrada);
+            resultado.Should().Be(resultadoEsperado);
+        }
+
+        [TestCase("IX", 9)]
+        [TestCase("XLIV", 44)]
+        [TestCase("LIX", 59)]
+        [TestCase("CDXLIV", 444)]
+        
+        public void deve_entender_numeros_romanos_menores_a_esquerda(string entrada, int resultadoEsperado)
+        {
+            var resultado = new Interpretador().Converter(entrada);
+            resultado.Should().Be(resultadoEsperado);
+        }
+    }
+}
